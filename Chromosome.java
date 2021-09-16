e93033f1bc969c3c4976debe810ad9180b8b8fd6
bryson <bryson158>
2021-09-16 16:03:13 -0400

    Now reads in the files needed

--- Chromosome.java
+++ Chromosome.java
@@ -4,7 +4,51 @@ import java.util.Random;
 public class Chromosome extends ArrayList<Item> implements Comparable<Chromosome>{
     private static Random rng;
 
+    //Empty constructor
     public Chromosome(){}
 
-    
+    //Constructor for the chromosome class
+    public Chromosome(ArrayList<Item> items){}
+
+    //Logic for determining a crossover between two chromosomes
+    public Chromosome crossover(Chromosome other){}
+
+    //Determines if the chromosome will mutate or not
+    public void mutate(){}
+
+    //Scores the fitness of the chromosome if the chromosome weighs more than 10 pounds the result will be 0
+    public int getFitness(){
+        double totalWeight = 0;
+
+        for(Item i: this){
+            if(i.isIncluded()){
+                totalWeight += i.getWeight();
+            }
+            if(totalWeight > 10){
+                return 0;
+            }
+        }
+        return (int) Math.round(totalWeight);
+    }
+
+    //Compares the chromosomes to each other
+    public int compareTo(Chromosome other){
+        return 0;
+    }
+
+    //TODO- display all items included in the chromosome
+    public String toString(){
+        String returnedString = "";
+
+        for(Item i: this){
+            if(i.isIncluded() == true){
+                returnedString += i.toString();
+            }
+        }
+
+        if(returnedString == ""){
+            return "There's no items in this chromosome";
+        }
+        return returnedString;
+    }
 }
--- GeneticAlgorithm.java
+++ GeneticAlgorithm.java
@@ -1,5 +1,34 @@
-public class GeneticAlgorithm {
-    public static void main(String[] args){
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class GeneticAlgorithm {
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<Item> items = readData("items.txt");
+    }
+
+    //Reads in the file and adds all the items in the file to an array list
+    public static ArrayList<Item> readData(String filename) throws FileNotFoundException{
+        File itemsFile = new File(filename);
+        Scanner input = new Scanner(itemsFile);
+        input.useDelimiter(",|\\n");
+
+        ArrayList<Item> items = new ArrayList<>();
+
+        while (input.hasNext()){
+            String name = input.next();
+            Double weight = Double.parseDouble(input.next().trim());
+            int value = Integer.parseInt(input.next().trim());
+            items.add(new Item(name, weight, value));
+        }
+        return items;
+    }
+
+    //Builds out the initial population of chromosomes
+    public static ArrayList<Chromosome> initializePopulation(ArrayList<Item> items, int populationSize){
+        ArrayList<Chromosome> chromosomes = new ArrayList<>();
+
+        return chromosomes;
     }
 }
