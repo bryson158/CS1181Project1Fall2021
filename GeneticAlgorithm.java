7b749b58d939472d0c1cfabed7843167c91d7112
bryson <bryson158>
2021-09-16 19:25:11 -0400

    Finished turning in

--- GeneticAlgorithm.java
+++ GeneticAlgorithm.java
@@ -14,29 +14,40 @@ public class GeneticAlgorithm {
         ArrayList<Chromosome> population = new ArrayList<>(initializePopulation(items, 10));
 
         //Epochs loop
-        for(int i = 0; i < 10000; i++){
+        for(int i = 0; i < 5000; i++){
             population.sort(Chromosome::compareTo);
 
             ArrayList<Chromosome> childChromosomes = new ArrayList<>();
-
+            ArrayList<Chromosome> nextGeneration = new ArrayList<>();
             //Creates child chromosomes
             for(int p = 0; p < population.size(); p++){
                 childChromosomes.add(population.get(p).crossover(population.get(rng.nextInt(population.size()))));
             }
 
-            childChromosomes.sort(Chromosome::compareTo);
-            //Adds only the 10 most fit children to the population
-            for(int p = 0; p < 10; p++){
-                population.add(childChromosomes.get(p));
-            }
+            population.sort(Chromosome::compareTo);
 
             //Chooses around 10% of the population to mutate. Note this doesn't guarantee 10% of the population will
             // mutate but rather that on average 10% of the population will be chosen to mutate
-            for(Chromosome c: population) {
+            for(Chromosome c: childChromosomes) {
                 if (rng.nextInt(10) == 1) {
                     c.mutate();
                 }
             }
+            childChromosomes.sort(Chromosome::compareTo);
+
+            //Adds the children to the next generation
+            for(int p = 0; p < 10; p++){
+                nextGeneration.add(childChromosomes.get(p));
+            }
+
+            //Adds the 10 most fit chromosomes to the next generation
+            for(int p = 0; p < 10; p++){
+                nextGeneration.add(population.get(p));
+                System.out.println(nextGeneration.get(p).toString());
+            }
+            population = nextGeneration;
+
+            ;
         }
         population.sort(Chromosome::compareTo);
 
